7f83e1b73483141199657021059b32b48db99be1
amriteshx1 <amriteshx18@gmail.com>
2025-03-16 21:17:20 +0530

    Add GeneralInfo component with input fields

--- cv-generator/src/App.jsx
+++ cv-generator/src/App.jsx
@@ -1,7 +1,24 @@
+function GeneralInfo(){
+  return (
+    <div className="ginfo">
+    <h2>General Information</h2>
 
+    <div className="gdetails">
+      <label htmlFor="name">Name</label>
+      <input type="text" name="user-name" id="name" />
+    </div>
 
-function App() {
-  return <h1>Let's start the grind!</h1>
+    <div className="gdetails">
+      <label htmlFor="email">Email</label>
+      <input type="email" name="user-email" id="email" />
+    </div>
+    
+    <div className="gdetails">
+      <label htmlFor="phone">Contact</label>
+      <input type="tel" name="user-phone" id="phone" />
+    </div>
+  </div>
+  )
 }
 
-export default App
+
