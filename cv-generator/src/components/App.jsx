ddf88da7a799d9ccad689a6a43915d502ec2523d
amriteshx1 <amriteshx18@gmail.com>
2025-03-17 13:04:07 +0530

    Integrated state management for user input in App component

--- cv-generator/src/components/App.jsx
+++ cv-generator/src/components/App.jsx
@@ -1,4 +1,5 @@
 import "../styles/App.css"
+import { useState } from "react";
 
 function GeneralInfo(){
   return (
@@ -7,22 +8,22 @@ function GeneralInfo(){
 
     <div className="gdetails">
       <label htmlFor="name">Name</label>
-      <input type="text" name="user-name" id="name" />
+      <input type="text" name="name" id="name" />
     </div>
 
     <div className="gdetails">
       <label htmlFor="profession">Profession</label>
-      <input type="text" name="user-profession" id="profession" />
+      <input type="text" name="profession" id="profession" />
     </div>
 
     <div className="gdetails">
       <label htmlFor="email">Email</label>
-      <input type="email" name="user-email" id="email" />
+      <input type="email" name="email" id="email" />
     </div>
     
     <div className="gdetails">
       <label htmlFor="phone">Contact</label>
-      <input type="tel" name="user-phone" id="phone" />
+      <input type="tel" name="phone" id="phone" />
     </div>
   </div>
   )
@@ -35,24 +36,24 @@ function Education(){
 
         <div className="edetails">
           <label htmlFor="school">School/College</label>
-          <input type="text" name="user-school" id="school" />
+          <input type="text" name="school" id="school" />
         </div>
 
         <div className="edetails">
           <label htmlFor="studyTitle">Degree/Course</label>
-          <input type="text" name="user-studyTitle" id="studyTitle" />
+          <input type="text" name="studyTitle" id="studyTitle" />
         </div>
 
         <div className="edetails">
           <div className="date">
             <div className="miniDate1">
             <label htmlFor="startDate">Start Date</label>
-            <input type="date" name="start-date" id="startDate" />
+            <input type="date" name="startDate" id="startDate" />
             </div>
 
             <div className="miniDate1">
             <label htmlFor="endDate">End Date</label>
-            <input type="date" name="end-date" id="endDate" />
+            <input type="date" name="endDate" id="endDate" />
             </div>
           </div>
         </div>
@@ -67,29 +68,29 @@ function Experience(){
 
         <div className="exdetails">
           <label htmlFor="company">Company Name</label>
-          <input type="text" name="cmp-name" id="company" />
+          <input type="text" name="company" id="company" />
         </div>
 
         <div className="exdetails">
           <label htmlFor="position">Job Title</label>
-          <input type="text" name="cmp-position" id="position" />
+          <input type="text" name="position" id="position" />
         </div>
 
         <div className="exdetails">
           <label htmlFor="responsibilities">Job Responsibilities</label>
-          <textarea id="responsibilities" name="cmp-responsibilities" rows={4} />
+          <textarea id="responsibilities" name="responsibilities" rows={4} />
         </div>
 
         <div className="exdetails">
           <div className="date2">
             <div className="miniDate2">
             <label htmlFor="startDate2">Start Date</label>
-            <input type="date" name="start-date2" id="startDate2" />
+            <input type="date" name="startDate2" id="startDate2" />
             </div>
 
             <div className="miniDate2">
             <label htmlFor="endDate2">End Date</label>
-            <input type="date" name="end-date2" id="endDate2" />
+            <input type="date" name="endDate2" id="endDate2" />
             </div>
           </div>
         </div>
@@ -105,13 +106,40 @@ function About(){
 
       <div className="adetails">
       <label htmlFor="about">Your Description</label>
-      <textarea name="user-about" id="about" rows={4} />
+      <textarea name="about" id="about" rows={4} />
       </div>
     </div>
   )
 }
 
 function App() {
+  
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    profession: "",
+    email: "",
+    phone: "",
+  });
+
+  const [education, setEducation] = useState({
+    school: "",
+    studyTitle: "",
+    startDate: "",
+    endDate: "",
+  });
+
+  const [experience, setExperience] = useState({
+    company: "",
+    position: "",
+    responsibilities: "",
+    startDate2: "",
+    endDate2: "",
+  });
+
+  const [about, setAbout] = useState({
+    about: "",
+  })
+
   return(
     <div className="container">
       <form>
