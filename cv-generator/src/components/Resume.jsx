ed9de5d634e891a1e0cf34838aa5f52f63e668c6
amriteshx1 <amriteshx18@gmail.com>
2025-03-17 16:32:21 +0530

    Add Header component for Resume to display data

--- cv-generator/src/components/Resume.jsx
+++ cv-generator/src/components/Resume.jsx
@@ -1,3 +1,11 @@
+function Header({generalInfo}){
+    return (
+        <div className="header">
+            <h1>{generalInfo.name}</h1>
+            <h2>{generalInfo.profession}</h2>
+        </div>
+    )
+}
 
 
 
